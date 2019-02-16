39b6e1f84fa336cdf117e4ea4cf35654e50dab93
Doreen Li <doreenli@DoreendeMacBook-2.local>
2019-02-16 10:19:25 -0800

    Code changed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.*;
+import frc.robot.commands.DeployClaw;
 import frc.robot.subsystems.*;
 
 public class Robot extends TimedRobot {
@@ -20,32 +21,12 @@ public class Robot extends TimedRobot {
     }
 
     @Override
-    public void robotPeriodic() {}
-
-    @Override
-    public void disabledInit() {}
-
-    @Override
-    public void disabledPeriodic() {
-        Scheduler.getInstance().run();
+    public void teleopInit() {
+        Scheduler.getInstance().add(new DeployClaw());
     }
-
-    @Override
-    public void autonomousInit() {}
-
-    @Override
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
-
-    @Override
-    public void teleopInit() {}
 
     @Override
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
     }
-
-    @Override
-    public void testPeriodic() {}
 }
